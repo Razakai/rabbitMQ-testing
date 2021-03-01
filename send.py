c30e37c015c7d0a7936270e725272522b1059a2a
Razakai <56027711+Razakai@users.noreply.github.com>
2021-03-01 19:40:45 +0000

    testing ocr and smtp

--- send.py
+++ send.py
@@ -1,5 +1,8 @@
 import pika
 import json
+import numpy as np
+import cv2
+from PIL import Image
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
@@ -7,13 +10,23 @@ channel = connection.channel()
 channel.queue_declare(queue='testingDetection')
 channel.queue_declare(queue='hello')
 channel.queue_declare(queue='baka')
+channel.queue_declare(queue='baka2')
+
+
+image = Image.open('65.jpg').convert('RGB')
+image = np.array(image)
+image = cv2.resize(image, (200, 50), interpolation=cv2.INTER_AREA)
+
+image = (cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) / 127.5) - 1.0
+image = [image.tolist()]
+
 
 
 
 channel.basic_publish(exchange='',
-                      routing_key='testingDetection',
-                      body=json.dumps({"message": "http://192.168.1.103:1234/video"}))
-                      #body="http://192.168.1.103:1234/video")
+                      routing_key='baka2',
+                      body=json.dumps({"detections": str(image), "city": "Cork", "country": "Ireland"}))
+                    
 
 print(" [x] Sent message")
 
